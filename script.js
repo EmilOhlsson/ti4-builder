a76f96ced74aba5e223b8d52f52d83da01618793
Emil Ohlsson <kotte@kottland.net>
2022-12-15 19:40:14 +0100

    Experimenting

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 console.log('Hello, world')
 
-function foo() {
-    alert("Submit button clicked");
-    return true;
+class FooBar {
+    foo() {
+        alert("Submit button clicked");
+        return true;
+    }
+
+    bar() {
+        //greeting = document.getElementById("text")
+        //txt = document.createTextNode("some text")
+        //greeting.appendChild(txt);
+        document.form.result = "42";
+    }
 }
 
 /* vim: set et ts=4 sw=4 ss=4 tw=100 : */
